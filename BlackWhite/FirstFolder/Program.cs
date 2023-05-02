de69f251087c901236fbf29d6ed8b4f5243e5d50
nizam <nizam@DESKTOP-TT8HCUN>
2023-05-03 03:04:08 +0400

    ilk commit

---
+++ BlackWhite/FirstFolder/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace BlackWhite.FirstFolder   //Kodlarimizi yazacagimiz bir alandir.
+{
+    internal class Program        // Clasimizi tanimliyoruz.
+    {
+        // DaysOfWeek enum tanımı Program sınıfının içinde yer almalıdır
+        enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            // Kodunuz buraya yazılmalıdır
+        }
+  
+    
+    }
+}
---
+++ BlackWhite/SecondFolder/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackWhite.SecondFolder
+{
+    public class Class1
+    {
+    
+    
+    
+    }
+
+
+
+}
